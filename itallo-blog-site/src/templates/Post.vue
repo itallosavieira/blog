7f2d32aa23f6f347ce53010680443bc5f9deae84
itallosavieira <itallo.sa.vieira@gmail.com>
2021-11-04 18:59:08 -0300

    Add Post structure

--- itallo-blog-site/src/templates/Post.vue
+++ itallo-blog-site/src/templates/Post.vue
@@ -1,8 +1,25 @@
-<template></template>
+<template>
+  <Layout>
+    <header class="title">
+      <h1>{{ $page.post.title }}</h1>
+    </header>
+    <main class="content" v-html="$page.post.content"></main>
+  </Layout>
+</template>
 
-<script>
-export default {};
-</script>
+<page-query>
+	query Post($path: String!) {
+		post: post(path: $path) {
+			id 
+			title 
+			path
+			content
+		}
+	}
+</page-query>
 
-<style>
+<style scoped>
+.title {
+  text-align: center;
+}
 </style>
