b188b417ce0aeb7e45bd3712819c3de238842a21
Itallo <itallo@Itallos-MacBook-Pro.local>
2022-03-24 09:19:59 -0300

    Remove old project

--- itallo-blog-site/src/main.js
+++
@@ -1,7 +0,0 @@
-
-import DefaultLayout from '~/layouts/App.vue'
-
-export default function (Vue, { router, head, isClient }) {
-  // Set default layout as a global component
-  Vue.component('Layout', DefaultLayout)
-}
